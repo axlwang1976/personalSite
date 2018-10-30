61e60506d96d2df0bf5bdf27403b8810a921c509
Axl Wang <axlwang1976@gmail.com>
2018-10-30 15:01:20 +0800

    20181030

--- src/js/function.js
+++ src/js/function.js
@@ -20,11 +20,13 @@ navBtns[3].addEventListener('click', () => {
 });
 
 // header
-window.addEventListener("scroll", function() {
+window.addEventListener('scroll', function() {
+    const top = this.pageYOffset;
+    const header = document.querySelector('.header__nav');
 
-    var top = this.scrollY;
-
-    var verticalScroll = document.querySelector(".verticalScroll");
-
-    console.log("Scroll X: " + top + "px");
+    if (top > 80) {
+        header.classList.add('header__nav-fixed');
+    } else if (top === 0) {
+        header.classList.remove('header__nav-fixed');
+    }
 });
