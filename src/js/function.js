4f4fb56ea3c380065622fce8e003e9bbe80820fe
Axl Wang <axlwang1976@gmail.com>
2018-11-01 14:44:43 +0800

    eslint

--- src/js/function.js
+++ src/js/function.js
@@ -5,36 +5,34 @@ const navBtns = document.querySelectorAll('.header__nav-link');
 
 navBtns[0].addEventListener('click', () => {
     scrollToElement('.about');
-})
+});
 navBtns[0].addEventListener('touchstart', () => {
     scrollToElement('.about');
-})
+});
 
 navBtns[1].addEventListener('click', () => {
     scrollToElement('.skill');
-})
+});
 navBtns[1].addEventListener('touchstart', () => {
     scrollToElement('.skill');
-})
+});
 
 navBtns[2].addEventListener('click', () => {
     scrollToElement('.work');
-})
+});
 navBtns[2].addEventListener('touchstart', () => {
     scrollToElement('.work');
-})
+});
 
 navBtns[3].addEventListener('click', () => {
     scrollToElement('.contact');
-})
+});
 navBtns[3].addEventListener('touchstart', () => {
     scrollToElement('.contact');
-})
-
-
+});
 
 // header
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const top = this.pageYOffset;
     const header = document.querySelector('.header__nav');
 
